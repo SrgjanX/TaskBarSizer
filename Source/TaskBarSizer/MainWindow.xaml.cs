deb890d5a94aea1541c1db7629bbd5e80a82c233
Srgjan Ristovski <csristovski@yahoo.com>
2022-08-30 16:05:21 +0200

    Initial commit.

---
+++ Source/TaskBarSizer/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace TaskBarSizer
+{
+    public partial class MainWindow : Window
+    {
+        private const string TaskBarRegistryKeyName = "TaskbarSi";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (RegistryManager regMgr = new RegistryManager())
+            {
+                regMgr.OnRegistyManagerErrorOccurred += RegMgr_OnRegistyManagerErrorOccurred;
+                string val = regMgr.Read(TaskBarRegistryKeyName);
+                switch (val)
+                {
+                    case "0":
+                        radio0.IsChecked = true;
+                        break;
+                    case "1":
+                        radio1.IsChecked = true;
+                        break;
+                    case "2":
+                        radio2.IsChecked = true;
+                        break;
+                }
+            }
+        }
+
+        private void RegMgr_OnRegistyManagerErrorOccurred(Exception ex, string methodName)
+        {
+            MessageBox.Show(ex.Message, methodName, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void ButtonSet_Click(object sender, RoutedEventArgs e)
+        {
+            void SetValue(int val)
+            {
+                using (RegistryManager regMgr = new RegistryManager())
+                    regMgr.WriteDword(TaskBarRegistryKeyName, val);
+                RestartExplorer();
+            }
+
+            if (radio0.IsChecked == true)
+            {
+                SetValue(0);
+            }
+            else if (radio1.IsChecked == true)
+            {
+                SetValue(1);
+            }
+            else if (radio2.IsChecked == true)
+            {
+                SetValue(2);
+            }
+        }
+
+        private void RestartExplorer()
+        {
+            try
+            {
+                foreach (Process process in Process.GetProcesses())
+                {
+                    try
+                    {
+                        if (process?.MainModule?.FileName?.ToLower()?.EndsWith(":\\windows\\explorer.exe") == true)
+                        {
+                            process.Kill();
+                            break;
+                        }
+                    }
+                    catch
+                    {
+                    }
+                }
+                //Process.Start("explorer.exe");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Could not restart explorer.exe.", "Error Occurred", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ Source/TaskBarSizer/RegistryManager.cs
@@ -0,0 +1,112 @@
+﻿//srgjanx
+
+using Microsoft.Win32;
+using System;
+using System.Reflection;
+
+namespace TaskBarSizer
+{
+    internal class RegistryManager : IDisposable
+    {
+        public delegate void RegistyManagerErrorEventHandler(Exception ex, string methodName);
+        public event RegistyManagerErrorEventHandler OnRegistyManagerErrorOccurred;
+
+        //Objects:
+        private RegistryKey BaseRegistryKey = Microsoft.Win32.Registry.CurrentUser;
+        private readonly string RegistryKey64 = "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced";
+
+        ~RegistryManager()
+        {
+            Dispose(false);
+        }
+
+        public string GetRegistryKey
+        {
+            get
+            {
+                return RegistryKey64;
+            }
+        }
+
+        public RegistryKey OpenRegistryKey
+        {
+            get
+            {
+                try
+                {
+                    return BaseRegistryKey.OpenSubKey(GetRegistryKey);
+                }
+                catch (NullReferenceException ex)
+                {
+                    throw new NullReferenceException($"Registry key '{GetRegistryKey}' does not exists. Reason: {ex.Message}");
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public void WriteDword(string key, object value)
+        {
+            try
+            {
+                if (value != null)
+                {
+                    BaseRegistryKey.CreateSubKey(RegistryKey64).SetValue(key, value, RegistryValueKind.DWord);
+                }
+            }
+            catch (Exception ex)
+            {
+                OnRegistyManagerErrorOccurred?.Invoke(ex, MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        public string Read(string key)
+        {
+            try
+            {
+                RegistryKey subKey = OpenRegistryKey;
+                if (subKey == null)
+                    throw new Exception($"Sub key {RegistryKey64} does not exists.");
+                else
+                {
+                    return subKey.GetValue(key) != null ? subKey.GetValue(key).ToString() : null;
+                }
+            }
+            catch (Exception ex)
+            {
+                OnRegistyManagerErrorOccurred?.Invoke(ex, MethodBase.GetCurrentMethod().Name);
+            }
+            return null;
+
+        }
+
+        #region IDisposable Pattern
+        // Flag: Has Dispose already been called?
+        private bool disposed = false;
+
+        // Public implementation of Dispose pattern callable by consumers.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+
+            }
+
+            BaseRegistryKey = null;
+            disposed = true;
+        }
+        #endregion
+    }
+}
